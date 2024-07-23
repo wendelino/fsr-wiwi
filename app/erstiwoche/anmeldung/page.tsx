3e13c3c097fbc85f1070138dbd2b43e2240594cb
Wendelin Beddermann <jwbklee@icloud.com>
2024-07-23 15:44:56 +0200

    nice animation Ameldeformular

--- app/erstiwoche/anmeldung/page.tsx
+++ app/erstiwoche/anmeldung/page.tsx
@@ -1,8 +1,6 @@
 
-"use client"
 import { RegisterForm } from "@/components/RegisterForm";
 import { AnimatedEvents, AnimatedText, Header } from "@/components/TextComponents";
-import { useEffect, useState } from "react";
 
 export default function page() {
 
@@ -11,22 +9,7 @@ export default function page() {
     {label: "Speedmeeting"},
     {label: "Stadtrallye"},
     {label: "Ersti-Party"}
-  ]
-
-  const [currentEvent, setCurrentEvent] = useState(events[0].label); // Initiale Veranstaltung
-
-  useEffect(() => {
-    // Index für die aktuelle Veranstaltung
-    let eventIndex = 0;
-
-    const intervalId = setInterval(() => {
-      eventIndex = (eventIndex + 1) % events.length; // Nächsten Index, Loop zurück zum Anfang
-      setCurrentEvent(events[eventIndex].label);
-    }, 2500); // Alle 3 Sekunden
-
-    // Aufräumarbeiten, um den Interval-Handler zu stoppen, wenn die Komponente unmontiert wird
-    return () => clearInterval(intervalId);
-  }, [events]);
+  ] 
 
   return (
     <div className="flex flex-col items-center">
