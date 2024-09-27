084c7d712c7b9de1fd3c7b835ab5ce88cb6904e0
wendelino <143810098+wendelino@users.noreply.github.com>
2024-09-27 21:56:18 +0200

    Event.tsx aktualisieren

--- components/Event.tsx
+++ components/Event.tsx
@@ -108,7 +108,7 @@ export default function EventCard({ event }: { event: EventWithLocation }) {
           )}
 
           {event.rest_seats == 0 && (
-            <div className="absolute rounded-lg inset-0 bg-background/80 flex justify-center items-center text-2xl font-bold text-foreground ">
+            <div className="absolute rounded-lg -z-1 inset-0 bg-background/80 flex justify-center items-center text-2xl font-bold text-foreground ">
               Ausgebucht!
             </div>
           )}
