02fc641c22b4f5ce8cf500e21b59e41d742ad20e
wendelino <jwbklee@icloud.com>
2024-10-31 21:57:47 +0100

    stable static

--- app/[lang]/mitglieder/route.ts
+++ app/[lang]/mitglieder/route.ts
@@ -1,6 +1,8 @@
-"use server";
+"use server"
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest) { 
-  return NextResponse.redirect(request.nextUrl);
+export async function GET(request: NextRequest) {
+  const url = new URL(request.url);
+  url.pathname += "/2024";
+  return NextResponse.redirect(url);
 }
