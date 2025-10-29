3f769690b9f775c2da04a65e6a77562b69ee6986
wendelino <jwbklee@icloud.com>
2025-10-29 14:41:59 +0100

    Update changes

--- app/(lang)/mensa/page.tsx
+++ app/(lang)/mensa/page.tsx
@@ -1,9 +1,12 @@
 import { getMensaMeals } from "@/app/_actions/mensa";
 import MensaView from "./view";
+import { notFound } from "next/navigation";
 
 export default async function MensaPage() {
 
     const data = await getMensaMeals("2025-10-16");
+
+    return notFound();
 
     if (!data || data.length === 0) {
         return <div>No data</div>;
