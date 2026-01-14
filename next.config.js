dde20a24ef315229769777fb82df91048f3c35bd
wendelino <jwbklee@icloud.com>
2026-01-14 20:48:33 +0100

    adding s3 endpoint to img remote patterns

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
 const nextConfig = {
   images: {
     remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
+      ...[NEXT_PUBLIC_SERVER_URL, "https://s3-api.m.lnio.de"].map((item) => {
         const url = new URL(item)
 
         return {
